25b105f2d493e79d7ce7f388b665e88fdb7a9270
EmirSilva <emir.silva@usach.cl>
2025-06-08 02:29:21 -0400

    cambios

--- m5-reservation-service/src/main/java/Backend/m5_reservation_service/repositories/KartRepository.java
+++ m5-reservation-service/src/main/java/Backend/m5_reservation_service/repositories/KartRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 @Repository
 public interface KartRepository extends JpaRepository<KartEntity, Long> {
+    //metodo para buscar un KartEntity por su codigo
+    KartEntity findByCode(String code);
     //metodo para buscar todos los KartEntity cuyo atributo "isAvailable" es verdadero
     List<KartEntity> findByIsAvailableTrue();
 }
