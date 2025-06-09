8b0702a7af0ee2df9dc3756d26484f7623ad7008
EmirSilva <emir.silva@usach.cl>
2025-06-08 23:35:02 -0400

    cambios

---
+++ m7-report-service/src/main/java/Backend/m7_report_service/config/RestTemplateConfig.java
@@ -0,0 +1,17 @@
+package Backend.m7_report_service.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced // ¡Esta anotación es CRUCIAL!
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+
